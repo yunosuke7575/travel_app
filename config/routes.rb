628a724e4b57237b10b9be963e8ca9f45049bce9
Yunosuke Katayama <purio.k1231@gmail.com>
2017-04-12 04:47:09 +0900

    index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/top'
+  resources :guides
+  root 'home#top'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
