4b3499fdf1d514b577620a0fbc43c085176e5f2d
Yunosuke Katayama <purio.k1231@gmail.com>
2017-04-29 08:39:20 +0900

    add link to users_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170427202903) do
+ActiveRecord::Schema.define(version: 20170428232100) do
 
   create_table "guides", force: :cascade do |t|
     t.string   "title"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170427202903) do
     t.string   "unconfirmed_email"
     t.string   "provider"
     t.string   "uid"
+    t.string   "link"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
