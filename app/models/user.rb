089ce646de37f7d283576fe33b56634dd8925941
Yunosuke Katayama <purio.k1231@gmail.com>
2017-04-30 11:06:47 +0900

    destroy all date when user deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :guides
+  has_many :guides, dependent: :destroy 
   
 end
