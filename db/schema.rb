0ed6dd24d119237bfbcaf7562e4f015592fe9b96
Yunosuke Katayama <purio.k1231@gmail.com>
2017-05-05 06:03:20 +0900

    update db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422221124) do
+ActiveRecord::Schema.define(version: 20170428232100) do
 
   create_table "guides", force: :cascade do |t|
     t.string   "title"
@@ -40,6 +40,9 @@ ActiveRecord::Schema.define(version: 20170422221124) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "link"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
