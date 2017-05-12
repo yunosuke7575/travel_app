c88dbd447182965956002627a550987304266e27
Yunosuke Katayama <purio.k1231@gmail.com>
2017-05-13 05:58:10 +0900

    enable to get link

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,21 +11,24 @@ class User < ApplicationRecord
   has_many :guides
   
   def self.new_with_session(params, session)
-  super.tap do |user|
-    if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-      user.email = data["email"] if user.email.blank?
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
     end
   end
-end
 
-def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
-    user.name = auth.info.name   # assuming the user model has a name
-    user.remote_image_url = auth.info.image # assuming the user model has an image
-    user.link = auth.info.urls # assuming the user model has a link 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.remote_image_url = auth.info.image # assuming the user model has an image
+      user.link = auth.info.urls # assuming the user model has a link 
+    end
   end
-end
   
+  def get_link
+    self.link.slice(31..91)
+  end
 end
