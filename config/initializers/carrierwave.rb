2f60e83791ad06d9908d714a9cdbbd4a2c459832
Yunosuke Katayama <purio.k1231@gmail.com>
2017-05-26 07:33:07 +0900

    s3に画像をアップロードするために、gem'fog'をインストール。image_uploaderno のstorage:fogに変更。config/initializers以下にcarrierwave.rbを作成

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,12 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: 'AKIAJC5PCZ6DY44ZKP7A',
+    aws_secret_access_key: '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',
+    region: 'us-west-1'
+  }
+
+  config.fog_directory  = 'travelapp-images'
+  config.cache_storage = :fog
+  
+end
