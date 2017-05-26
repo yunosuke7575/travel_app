57b39914012abb322adbea63ec179414d01c9033
Yunosuke Katayama <purio.k1231@gmail.com>
2017-05-26 13:55:25 +0900

    securityのためcarrierwave.rbからkeyを削除

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AKIAJC5PCZ6DY44ZKP7A',
-    aws_secret_access_key: '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',
+    aws_access_key_id: ENV['ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
     region: 'us-west-1'
   }
 
